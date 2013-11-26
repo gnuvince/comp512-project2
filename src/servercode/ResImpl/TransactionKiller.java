cbe8c6c61a1ef71152f451bdd434a558245d7649
Vincent Foley <vfoley@gmail.com>
2013-11-26 10:50:30 -0500

    Added report to repository

--- src/servercode/ResImpl/TransactionKiller.java
+++ src/servercode/ResImpl/TransactionKiller.java
@@ -7,24 +7,22 @@ public class TransactionKiller implements Runnable {
 
 	private TransactionManager txnManager;
 	private ResourceManagerImpl rm;
-	
+
 	public TransactionKiller(TransactionManager txnMan, ResourceManagerImpl rm){
 		txnManager = txnMan;
 		this.rm = rm;
 	}
-	
+
 	public void run() {
-        System.out.println("You better stay active or I'll kill you!!!");
-        
         while (true){
-        	
+
         	try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        	
+
         	Vector<Integer> transactionsToKill = txnManager.getTimedOutTransactions();
         	for(Integer txn: transactionsToKill){
         		try {
@@ -34,9 +32,9 @@ public class TransactionKiller implements Runnable {
 					e.printStackTrace();
 				}
         	}
-        	
+
         }
-        
+
     }
 
 }
